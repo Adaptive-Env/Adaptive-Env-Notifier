753ddfb375e13af8f41fb65899b551b9e1f7d14a
Wiarr3 <wiora.biznes@gmail.com>
2025-10-05 22:22:30 +0200

    work?

--- src/main/java/com/adaptive/environments/notify_service/notifier/MailAlertNotifier.java
+++ src/main/java/com/adaptive/environments/notify_service/notifier/MailAlertNotifier.java
@@ -9,6 +9,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @ConditionalOnProperty(prefix = "notifier.mail", name = "enabled", havingValue = "true")
 public class MailAlertNotifier implements AlertNotifier{
